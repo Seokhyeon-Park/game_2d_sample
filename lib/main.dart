918c0ad591910d049f4407869e240ef74ecf7e2c
seokhyeon park <dfbgq346q@gmail.com>
2023-03-10 15:55:34 +0900

    계정 생성 화면 추가

--- lib/main.dart
+++ lib/main.dart
@@ -5,37 +5,81 @@ import 'package:flutter/material.dart';
 import 'package:game_2d_sample/main_game.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: Stack(
-          children: [
-            MainGamePage(),
-          ],
-        ),
-      ),
-    )
-  );
+  runApp(const MyApp());
 }
 
-class MainGamePage extends StatefulWidget {
-  const MainGamePage({Key? key}) : super(key: key);
-
-  @override
-  State<MainGamePage> createState() => _MainGamePageState();
-}
-
-class _MainGamePageState extends State<MainGamePage> {
-  late final MainGame _mainGame;
-
-  @override
-  void initState() {
-    super.initState();
-    _mainGame = MainGame();
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GameWidget(game: _mainGame);
+    return MaterialApp(
+      initialRoute: '/createAccount',
+      routes: {
+        '/createAccount': (context) => const CreateAccount(),
+      },
+      home: const MainGamePage(),
+    );
+  }
+}
+
+
+class MainGamePage extends StatelessWidget {
+  const MainGamePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GameWidget(game: MainGame()),
+    );
+  }
+}
+
+class CreateAccount extends StatelessWidget {
+  const CreateAccount({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          color: Colors.blueGrey,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Your name?',
+                    hintStyle: const TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(width: 2.0, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(width: 2.0, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.check_circle_outline_rounded,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
